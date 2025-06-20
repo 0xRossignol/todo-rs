0e388370f67eb1bd58971d30f62585c539c1d785
rossignol <2746054243@qq.com>
2025-06-20 15:29:41 +0800

    feat: basic cli args process

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use std::{env, process};
+
 fn main() {
-    println!("Hello, world!");
+    // 获取命令行参数
+    let args: Vec<String> = env::args().collect();
+
+    // 检查是否有足够的参数
+    if args.len() < 2 {
+        eprintln!("Usage: rodo [command] [options]");
+        process::exit(1);
+    }
+
+    let command = &args[1];
+    match command.as_str() {
+        "add" => {
+            if args.len() < 3 {
+                eprintln!("Usage: rodo add [contents]\n");
+                process::exit(1);
+            }
+
+            println!("Adding: {}", args[2]);
+        }
+        "rm" => {
+            if args.len() < 3 {
+                eprintln!("Usage: rodo rm [contents]\n");
+                process::exit(1);
+            }
+
+            println!("Removing: {}", args[2]);
+        }
+        "ls" => {
+            println!("Listing all items...");
+        }
+
+        _ => {
+            eprintln!("Unknown command: {}\n", command);
+            process::exit(1);
+        }
+    }
+    
 }
