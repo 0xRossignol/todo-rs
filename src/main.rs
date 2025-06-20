a7c0864c48032c16c93536074fa22dba43cbfc00
rossignol <2746054243@qq.com>
2025-06-20 17:29:58 +0800

    feat: add the list logic and impl the inc id logic

--- src/main.rs
+++ src/main.rs
@@ -26,7 +26,7 @@ fn main() {
             }
 
             let contents = &args[2..].join(" ");
-            let id = 1;
+            let id = db.read_tasks().unwrap().last().map(|task| task.id + 1).unwrap_or(0);
             let status = Status::TODO;
             if let Err(e) = db.add_task(task::Task {
                 id,
@@ -45,7 +45,23 @@ fn main() {
             println!("Removing: {}", args[2]);
         }
         "ls" => {
-            println!("Listing all items...");
+            let tasks = db.read_tasks().unwrap_or_else(|err| {
+                eprintln!("Error reading tasks: {}", err);
+                process::exit(1);
+            });
+            if tasks.is_empty() {
+                eprintln!("No tasks found, you can add one using `rodo add <task>`");
+                return;
+            }
+
+            for task in tasks {
+                match task.status {
+                    Status::TODO => println!("[]\t{}\t{}", task.id, task.content),
+                    Status::IN_PROGRESS => println!("[*]\t{}\t{}", task.id, task.content),
+                    Status::DONE => println!("[Y]\t{}\t{}", task.id, task.status),
+                    _ => println!("[ELSE]\t{}\t{}", task.id, task.status)
+                }
+            }
         }
 
         _ => {
